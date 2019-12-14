3beddfb1005ffe9066bd7839b5e25af410ef3fb3
Paul Anderson <paul@grammatech.com>
2019-12-14 17:35:58 +0000

    This is the first version to go live on Weebly.

--- files/theme/js/map.js
+++ files/theme/js/map.js
@@ -138,7 +138,7 @@ function testScalability(mymap) {
 function mapMain() {
     var mymap = L.map('mapid').setView([42.4595, -76.4866], 14);
 
-    L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+    L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiZmlzaG9hayIsImEiOiJjazQ1cGp0dmswOWQ3M2Z0bHQ2OW9kZGFyIn0.DzjkdSMt23L_TrN_5I-Ufg', {
         maxZoom: 18,
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
             '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
